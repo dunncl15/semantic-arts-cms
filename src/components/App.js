b84b9720f88061e60c06a3e03f35c6d2f7e63bfb
Charles Dunn <dunnc1551@gmail.com>
2017-04-10 17:25:59 -0600

    Import Navigation component

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
-// import './App.css';
+import Navigation from './Navigation';
 
 class App extends Component {
   render() {
     return (
       <main className="App">
         <h1>Test</h1>
+        <Navigation />
       </main>
     );
   }
