37ea696a62671ba792eb52441ac1e60db653c1d9
Charles Dunn <dunnc1551@gmail.com>
2017-04-10 17:23:21 -0600

    Add Navigation component with basic routing and styling

---
+++ src/components/Navigation.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const Navigation = () => {
+  return (
+    <aside className='nav-bar'>
+     <ul>
+      <NavLink exact to='/'
+               className="menu-item"
+               activeClassName='active'>
+               <li>Dashboard</li>
+      </NavLink>
+      <NavLink to='/pages'
+               className="menu-item"
+               activeClassName='active'>
+               <li>Pages</li>
+      </NavLink>
+      <NavLink to='/new-page'
+               className="menu-item"
+               activeClassName='active'>
+               <li>Create Page</li>
+      </NavLink>
+     </ul>
+    </aside>
+  )
+}
+
+export default Navigation;
