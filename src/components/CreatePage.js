3f7800249fa7d99d942b7221a9655b3889721c1b
Charles Dunn <dunnc1551@gmail.com>
2017-04-10 19:18:06 -0600

    Create component and container for new pages

---
+++ src/components/CreatePage.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class CreatePage extends Component {
+
+  render() {
+    return (
+      <section>
+        <h3>Create Page</h3>
+      </section>
+    )
+  }
+}
