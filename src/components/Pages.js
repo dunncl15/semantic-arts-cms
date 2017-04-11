d4f7e76c784b1bb12662e7ae1cde8afb5f0f006f
Charles Dunn <dunnc1551@gmail.com>
2017-04-10 18:22:01 -0600

    Create Pages component and Container

---
+++ src/components/Pages.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class Pages extends Component {
+  render() {
+    return (
+      <h3>Pages</h3>
+    )
+  }
+}
---
+++ src/containers/PagesContainer.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+import Pages from '../components/Pages.js';
+
+
+export default connect(null, null)(Pages);
