1646acd1b185f158810dcb8f55de4f41196a281b
Charles Dunn <dunnc1551@gmail.com>
2017-04-10 18:19:33 -0600

    Import PagesContainer and addroute to display component

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
+
 import Navigation from './Navigation';
+import PagesContainer from '../containers/PagesContainer.js';
+
 
 class App extends Component {
   render() {
@@ -7,6 +11,7 @@ class App extends Component {
       <main className="App">
         <h1>Test</h1>
         <Navigation />
+        <Route exact path="/pages" component={ PagesContainer }></Route>
       </main>
     );
   }
