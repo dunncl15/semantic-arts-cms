d3d23fb8e1fd3149445ca58ead35fa9827bd2575
Charles Dunn <dunnc1551@gmail.com>
2017-04-11 18:57:55 -0600

    Add basic form to CreatePage component

--- src/components/CreatePage.js
+++ src/components/CreatePage.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
+import RichTextEditor from 'react-rte';
 
 export default class CreatePage extends Component {
 
   render() {
     return (
-      <section>
+      <section className="create-page-wrap">
         <h3>Create Page</h3>
+        <form className='create-page'>
+          <label htmlFor='title'>Title:</label>
+          <input type='text' id='title' />
+          <label htmlFor='body'>Body:</label>
+          <textarea type='text' id='body'></textarea>
+        </form>
       </section>
     )
   }
