50e21b9d629e1536d58035258e3e896522e232a3
Charles Dunn <dunnc1551@gmail.com>
2017-04-11 21:16:52 -0600

    Add state to CreatePage component to capture user input

--- src/components/CreatePage.js
+++ src/components/CreatePage.js
@@ -2,16 +2,54 @@ import React, { Component } from 'react';
 import RichTextEditor from 'react-rte';
 
 export default class CreatePage extends Component {
+  constructor() {
+    super();
+    this.state = {
+      title: '',
+      body: '',
+      published: false
+    }
+  }
+
+  handleChange(e) {
+    const { name, value } = e.target;
+    this.setState({
+      [name]: value
+    })
+  }
+
+  handleClick(e) {
+    e.preventDefault();
+    this.props.addPage(this.state);
+    this.setState({
+      title: '',
+      body: ''
+    })
+  }
 
   render() {
     return (
       <section className="create-page-wrap">
-        <h3>Create Page</h3>
+        <h2>Create Page</h2>
         <form className='create-page'>
           <label htmlFor='title'>Title:</label>
-          <input type='text' id='title' />
+          <input type='text'
+                 id='title'
+                 name='title'
+                 value={this.state.title}
+                 onChange={(e) => this.handleChange(e)}
+          />
           <label htmlFor='body'>Body:</label>
-          <textarea type='text' id='body'></textarea>
+          <textarea type='text'
+                    id='body'
+                    name='body'
+                    value={this.state.body}
+                    onChange={(e) => this.handleChange(e)}>
+          </textarea>
+          <button className='save-btn'
+                  onClick={(e) => this.handleClick(e)}>
+                  Save page
+          </button>
         </form>
       </section>
     )
