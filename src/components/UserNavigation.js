f8e5f02688a97e8f3a61e196f10d2370ee7cec75
Charles Dunn <dunnc1551@gmail.com>
2017-04-13 15:16:20 -0600

    Create client facing navigation component.

---
+++ src/components/UserNavigation.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom';
+
+const UserNavigation = () => {
+  return (
+    <div>
+     <NavLink to='/pages'>Test</NavLink>
+    </div>
+  )
+}
+
+export default UserNavigation;
