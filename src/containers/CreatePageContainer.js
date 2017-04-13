f3eba8f9efb4073befe4bb9bc21e34696a92a738
Charles Dunn <dunnc1551@gmail.com>
2017-04-13 14:45:41 -0600

    Add edit page action.

--- src/containers/CreatePageContainer.js
+++ src/containers/CreatePageContainer.js
@@ -6,6 +6,9 @@ const mapDispatchToProps = (dispatch) => {
   return {
     addPage: (page) => {
       dispatch(actions.addPage(page))
+    },
+    editPage: (page) => {
+      dispatch(actions.editPage(page))
     }
   }
 }
