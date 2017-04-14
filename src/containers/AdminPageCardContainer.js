3a883ba781f090b18af676b5527d9945e0d1f53a
Charles Dunn <dunnc1551@gmail.com>
2017-04-14 17:35:18 -0600

    Add togglePublish actionand pass state to AdminPageCardContainer.

--- src/containers/AdminPageCardContainer.js
+++ src/containers/AdminPageCardContainer.js
@@ -2,12 +2,19 @@ import { connect } from 'react-redux';
 import * as actions from '../actions/actions';
 import AdminPageCard from '../components/AdminPageCard.js';
 
+const mapStateToProps = (state) => {
+  return state;
+}
+
 const mapDispatchToProps = (dispatch) => {
   return {
     addToNav: (pageName) => {
       dispatch(actions.addToNav(pageName))
+    },
+    togglePublish: (page) => {
+      dispatch(actions.togglePublish(page))
     }
   }
 }
 
-export default connect(null, mapDispatchToProps)(AdminPageCard);
+export default connect(mapStateToProps, mapDispatchToProps)(AdminPageCard);
