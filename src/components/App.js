51918f4c8f4a29d3dd23832cbf6cc96521054eb8
Charles Dunn <dunnc1551@gmail.com>
2017-04-14 13:55:32 -0600

    Add dynamic routing for client facing pages.

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import { Route } from 'react-router-dom';
 import Navigation from './Navigation';
 import UserNavigationContainer from '../containers/UserNavigationContainer.js';
 import PagesContainer from '../containers/PagesContainer.js';
+import Page from './Page.js';
 import HeaderContainer from '../containers/HeaderContainer.js';
 import CreatePageContainer from '../containers/CreatePageContainer.js';
 
@@ -11,13 +12,19 @@ const App = ({ pages, history }) => {
 
   return (
     <main className='App'>
-      <Route exact path='/' component={ UserNavigationContainer }></Route>
+      <Route path='/' component={ UserNavigationContainer }></Route>
+      <Route path='/pages/:title' render={ ({ match }) => {
+        console.log(match);
+        const page = pages.find(page => page.title.toLowerCase() === match.params.title.toLowerCase());
+        return <Page page={page} />
+      }}></Route>
       <Route path='/admin' component={ HeaderContainer }></Route>
       <Route path='/admin' component={ Navigation }></Route>
       <Route exact path='/admin/pages' component={ PagesContainer }></Route>
       <Route exact path='/admin/new-page' component={ CreatePageContainer }></Route>
-      <Route path='/admin/pages/edit/:title' render={ ({ match }) => {
-        const page = pages.find(page => page.title === match.params.title);
+      <Route path='/admin/pages/edit/:id' render={ ({ match }) => {
+        console.log(match);
+        const page = pages.find(page => page.id === parseInt(match.params.id));
         return <CreatePageContainer page={page} history={history}/>
       }}></Route>
     </main>
