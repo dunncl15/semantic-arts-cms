5ec1c864f8f8a75795f6843bf688e9da6dca546c
Charles Dunn <dunnc1551@gmail.com>
2017-04-14 14:00:32 -0600

    Create new reducer to handle addToNav action.

--- src/reducers/userNavReducer.js
+++ src/reducers/userNavReducer.js
@@ -1,8 +1,9 @@
 export const userNav = (state = [], action) => {
   switch (action.type) {
     case 'ADD_TO_NAV':
-      console.log('clicked!');
-      return [...state, action.pageName];
+      console.log(action.page);
+      // const newPage = Object.assign({}, title: action.page.title, published: action.page.published)
+      return [...state, action.page];
       break;
     default:
       return state;
