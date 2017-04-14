f7882bf2f5272afec7d22b427e51341d9bc1758e
Charles Dunn <dunnc1551@gmail.com>
2017-04-14 13:58:05 -0600

    Rename PageCard component to AdminPageCard for clarity.

--- src/components/AdminPageCard.js
+++ src/components/AdminPageCard.js
@@ -2,24 +2,24 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import classNames from 'classnames';
 
-const PageCard = ({ title, content, published, addToNav, history }) => {
+const AdminPageCard = ({ title, content, published, id, addToNav, history }) => {
   const btnClass = classNames({
     'unpublish': published,
     'publish': !published
   })
   return (
-    <article className='page-card'>
+    <article className='page-card' id={id}>
       <h2 className='page-title'>{ title }</h2>
       <div className='page-body' dangerouslySetInnerHTML={{__html: content}}></div>
       <button className={btnClass}>{ published ? 'Unpublish' : 'Publish' }</button>
       <footer className='card-footer'>
-        <Link to={`pages/edit/${title}`}>Edit page</Link>
-        <button onClick={() => addToNav(title)}>Add to navigation</button>
-        <button onClick={() => history.push('/')}></button>
+        <Link to={`pages/edit/${id}`}>Edit page</Link>
+        <button onClick={ () => addToNav({ title, published: !published, id: id }) }>Add to navigation</button>
+        <button onClick={ () => history.push('/') }></button>
         <p className='status'><span>Status</span>: { published ? 'published' : 'unpublished' }</p>
       </footer>
     </article>
   )
 }
 
-export default PageCard;
+export default AdminPageCard;
