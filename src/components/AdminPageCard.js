b670fa5f4bf16a2ac837f7294eae92b9e1b3ae05
Charles Dunn <dunnc1551@gmail.com>
2017-04-14 17:33:45 -0600

    Add handlePublish helperfunction to update onclick events to call helper function.

--- src/components/AdminPageCard.js
+++ src/components/AdminPageCard.js
@@ -2,12 +2,17 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import classNames from 'classnames';
 
-const checkUserNav = ({ userNav, addToNav, title, published, id }) => {
+const handlePublish = ({ userNav, addToNav, togglePublish, title, published, id, content }) => {
   const index = userNav.findIndex(page => page.title === title);
-  index === -1 ? addToNav({ title, id, published: !published }) : alert(`A page with the title ${title} already exists.`);
+  if (index === -1) {
+    addToNav({ title });
+    togglePublish({ title, content, id, published })
+  } else {    
+    togglePublish({ title, content, id, published })
+  }
 }
 
-const AdminPageCard = ({ title, content, published, id, addToNav, history, userNav }) => {
+const AdminPageCard = ({ title, content, published, id, addToNav, togglePublish, history, userNav }) => {
   const btnClass = classNames({
     'unpublish': published,
     'publish': !published
@@ -16,13 +21,20 @@ const AdminPageCard = ({ title, content, published, id, addToNav, history, userN
     <article className='page-card' id={id}>
       <h2 className='page-title'>{ title }</h2>
       <div className='page-body' dangerouslySetInnerHTML={{__html: content}}></div>
-      <button className={btnClass}>{ published ? 'Unpublish' : 'Publish' }</button>
+      <button className={btnClass}
+              onClick={ () => togglePublish({ title, content, id, published }) }>
+              { published ? 'Unpublish' : 'Publish' }
+      </button>
       <footer className='card-footer'>
         <Link to={`pages/edit/${id}`}>Edit page</Link>
-        <button onClick={ () => checkUserNav({ userNav, addToNav, title, published, id }) }>Add to navigation</button>
-        <button onClick={ () => history.push('/') }></button>
-        <p className='status'><span>Status</span>: { published ? 'published' : 'unpublished' }</p>
+        <button onClick={ () => handlePublish({ userNav, addToNav, togglePublish, title, published, id, content }) }>
+          { published ? 'Remove from navigation' : 'Add to navigation'}
+        </button>
+        <p className='status'>
+          <span>Status</span>: { published ? 'published' : 'unpublished' }
+        </p>
       </footer>
+      <button onClick={() => history.push('/pages/about')}></button>
     </article>
   )
 }
