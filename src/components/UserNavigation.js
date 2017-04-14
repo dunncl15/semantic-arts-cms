fa7334ba195773a317798f47b993523146ab34c6
Charles Dunn <dunnc1551@gmail.com>
2017-04-14 13:59:53 -0600

    Add functionality to render page titles as links on user navigation.

--- src/components/UserNavigation.js
+++ src/components/UserNavigation.js
@@ -5,7 +5,7 @@ const UserNavigation = ({ userNav }) => {
   return (
     <nav className="user-nav">
       <NavLink className='home nav-item' to='/'>Home</NavLink>
-      {userNav.map((title, i) => <NavLink className='nav-item' key={i} to='/'>{ title }</NavLink>)}
+      {userNav.map((page, i) => <NavLink className='nav-item' key={i} to={`/pages/${page.title.toLowerCase()}`}>{ page.title }</NavLink>)}
     </nav>
   )
 }
