3e2b84b02444d36a3c0544bd255a5cd9cb2c6e25
Charles Dunn <dunnc1551@gmail.com>
2017-04-15 16:33:18 -0600

    Import svgs, add svgs to buttons to match comp.

--- src/components/AdminPageCard.js
+++ src/components/AdminPageCard.js
@@ -2,6 +2,11 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import classNames from 'classnames';
 
+import cloudPublish from '../styles/images/cloud-upload.svg';
+import cloudUnpublish from '../styles/images/cloud-unpublish.svg';
+import pencil from '../styles/images/pencil-blue.svg';
+import add from '../styles/images/add-icon.svg';
+
 const handleNav = ({ userNav, addToNav, removeFromNav, togglePublish, toggleNav, toggleNavPublish, title, content, id, published, navigation }) => {
   const index = userNav.findIndex(page => page.title === title);
   if (index === -1 && !published) {
@@ -31,22 +36,26 @@ const AdminPageCard = ({ userNav, addToNav, removeFromNav, togglePublish, toggle
   })
   return (
     <article className='page-card' id={id}>
-      <h2 className='page-title'>{ title }</h2>
+      <h3 className='page-title'>{ title }</h3>
       <div className='page-body' dangerouslySetInnerHTML={{__html: content}}></div>
       <button className={btnClass}
               onClick={ () => handlePublish({ userNav, addToNav, removeFromNav, togglePublish, toggleNav, toggleNavPublish, title, content, id, published, navigation })}>
+              { published ? <img className='cloud-icon' src={cloudUnpublish}/> : <img className='cloud-icon' src={cloudPublish}/>}
               { published ? 'Unpublish' : 'Publish' }
       </button>
       <footer className='card-footer'>
-        <Link to={`pages/edit/${title}`}>Edit page</Link>
-        <button onClick={ () => handleNav({ userNav, addToNav, removeFromNav, togglePublish, toggleNav, toggleNavPublish, title, content, id, published, navigation }) }>
-          { navigation ? 'Remove from navigation' : 'Add to navigation'}
+        <Link className='edit' to={`pages/edit/${title}`}>
+          <img className='pencil-blue' src={pencil}/>
+          Edit page
+          </Link>
+        <button className='add-to-nav-btn' onClick={ () => handleNav({ userNav, addToNav, removeFromNav, togglePublish, toggleNav, toggleNavPublish, title, content, id, published, navigation }) }>
+        <img className='add-icon' src={add} />
+          { navigation ? <span>Remove from navigation</span> : <span>Add to navigation</span> }
         </button>
         <p className='status'>
           <span>Status</span>: { published ? 'published' : 'unpublished' }
         </p>
       </footer>
-      <button onClick={() => history.push('/pages/about')}></button>
     </article>
   )
 }
