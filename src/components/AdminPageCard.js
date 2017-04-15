33a31d852d99d4ac4e3493e0b0b4d67d4e7fcd46
Charles Dunn <dunnc1551@gmail.com>
2017-04-15 12:33:21 -0600

    Add handleNav helper function to handle logic for all scenarios of adding/removing nav links along with publishing/unpublishing pages. Add handlePublish helper to handle logic for the publish/unpublish button on each page card.

--- src/components/AdminPageCard.js
+++ src/components/AdminPageCard.js
@@ -2,17 +2,29 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import classNames from 'classnames';
 
-const handlePublish = ({ userNav, addToNav, togglePublish, title, published, id, content }) => {
+const handleNav = ({ userNav, addToNav, removeFromNav, togglePublish, toggleNav, toggleNavPublish, title, content, id, published, navigation }) => {
   const index = userNav.findIndex(page => page.title === title);
-  if (index === -1) {
-    addToNav({ title });
-    togglePublish({ title, content, id, published })
-  } else {    
-    togglePublish({ title, content, id, published })
+  if (index === -1 && !published) {
+    addToNav({ title, content, id, published, navigation })
+    toggleNavPublish({ title, content, id, published, navigation });
+  }
+  if (index === -1 && published) {
+    addToNav({ title, content, id, published, navigation })
+    toggleNav({ title, content, id, published, navigation })
+  }
+  if (index !== -1 && published) {
+    removeFromNav({ title, content, id, published, navigation })
+    toggleNavPublish({ title, content, id, published, navigation });
   }
 }
 
-const AdminPageCard = ({ title, content, published, id, addToNav, togglePublish, history, userNav }) => {
+const handlePublish = ({ userNav, removeFromNav, togglePublish, toggleNavPublish, title, content, id, published, navigation }) => {
+  const index = userNav.findIndex(page => page.title === title);
+  index !== -1 && published ? toggleNavPublish({ title, content, id, published, navigation }) : togglePublish({ title, content, id, published, navigation });
+  removeFromNav({ title, content, id, published, navigation });
+}
+
+const AdminPageCard = ({ userNav, addToNav, removeFromNav, togglePublish, toggleNav, toggleNavPublish, title, content, id, published, navigation, history }) => {
   const btnClass = classNames({
     'unpublish': published,
     'publish': !published
@@ -22,13 +34,13 @@ const AdminPageCard = ({ title, content, published, id, addToNav, togglePublish,
       <h2 className='page-title'>{ title }</h2>
       <div className='page-body' dangerouslySetInnerHTML={{__html: content}}></div>
       <button className={btnClass}
-              onClick={ () => togglePublish({ title, content, id, published }) }>
+              onClick={ () => handlePublish({ userNav, addToNav, removeFromNav, togglePublish, toggleNav, toggleNavPublish, title, content, id, published, navigation })}>
               { published ? 'Unpublish' : 'Publish' }
       </button>
       <footer className='card-footer'>
-        <Link to={`pages/edit/${id}`}>Edit page</Link>
-        <button onClick={ () => handlePublish({ userNav, addToNav, togglePublish, title, published, id, content }) }>
-          { published ? 'Remove from navigation' : 'Add to navigation'}
+        <Link to={`pages/edit/${title}`}>Edit page</Link>
+        <button onClick={ () => handleNav({ userNav, addToNav, removeFromNav, togglePublish, toggleNav, toggleNavPublish, title, content, id, published, navigation }) }>
+          { navigation ? 'Remove from navigation' : 'Add to navigation'}
         </button>
         <p className='status'>
           <span>Status</span>: { published ? 'published' : 'unpublished' }
