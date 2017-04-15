ddba903d654b7601f0008548c55a32d3ec087fbf
Charles Dunn <dunnc1551@gmail.com>
2017-04-15 12:50:30 -0600

    Add logic to update state for new actions.

--- src/reducers/pagesReducer.js
+++ src/reducers/pagesReducer.js
@@ -1,4 +1,3 @@
-
 export const pages = (state = [], action) => {
   switch (action.type) {
     case 'ADD_PAGE':
@@ -12,12 +11,44 @@ export const pages = (state = [], action) => {
         ...state.slice(index + 1)
       ];
       break;
+    case 'EDIT_PUBLISHED':
+      const ind = state.findIndex(page => page.title === action.page.title);
+      return [
+        ...state.slice(0, ind),
+        {...action.page, published: true},
+        ...state.slice(ind + 1)
+      ];
+      break;
+    case 'EDIT_PUB_NAV':
+      const x = state.findIndex(page => page.title === action.page.title);
+      return [
+        ...state.slice(0, x),
+        {...action.page, published: true, navigation: true},
+        ...state.slice(x + 1)
+      ];
+      break;
     case 'TOGGLE_PUBLISH':
       const i = state.findIndex(page => page.title === action.page.title);
       return [
         ...state.slice(0, i),
         {...action.page, published: !action.page.published},
         ...state.slice(i + 1)
+      ];
+      break;
+    case 'TOGGLE_NAV':
+      const ix = state.findIndex(page => page.title === action.page.title);
+      return [
+        ...state.slice(0, ix),
+        {...action.page, navigation: !action.page.navigation},
+        ...state.slice(ix + 1)
+      ];
+      break;
+    case 'TOGGLE_NAV_PUBLISH':
+      const idx = state.findIndex(page => page.title === action.page.title);
+      return [
+        ...state.slice(0, idx),
+        {...action.page, published: !action.page.published, navigation: !action.page.navigation},
+        ...state.slice(idx + 1)
       ];
       break;
     default:
--- src/reducers/userNavReducer.js
+++ src/reducers/userNavReducer.js
@@ -1,7 +1,10 @@
 export const userNav = (state = [], action) => {
   switch (action.type) {
     case 'ADD_TO_NAV':
-      return [...state, action.page];
+      return [...state, action.title];
+      break;
+    case 'REMOVE_FROM_NAV':
+      return state.filter(page => page.title !== action.title.title);
       break;
     default:
       return state;
