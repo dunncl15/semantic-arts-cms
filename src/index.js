fc8030861a51250805febf81b641700cac53f77f
Charles Dunn <dunnc1551@gmail.com>
2017-04-15 12:45:48 -0600

    Remove unused import vars.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
-import PropTypes from 'prop-types';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 import createHistory from 'history/createBrowserHistory'
