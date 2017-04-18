36b18fdef5d22cfd00a99f4e423a49eb24d95458
Charles Dunn <dunnc1551@gmail.com>
2017-04-17 18:20:28 -0600

    Add unordered list format links added to navigation.

--- src/components/UserNavigation.js
+++ src/components/UserNavigation.js
@@ -1,11 +1,23 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom';
+import logo from '../styles/images/logo.png';
 
 const UserNavigation = ({ userNav }) => {
   return (
     <nav className="user-nav">
-      <NavLink className='home nav-item' to='/'>Home</NavLink>
-      {userNav.map((page, i) => <NavLink className='nav-item' key={i} to={`/pages/${page.title.toLowerCase()}`}>{ page.title }</NavLink>)}
+      <NavLink className='home' to='/admin'>
+        <img src={logo} alt='company logo' />
+      </NavLink>
+      <ul className='nav-links'>
+      {userNav.map((page, i) =>
+        <NavLink className='nav-item'
+                 activeClassName='active'
+                 key={i}
+                 to={`/pages/${page.title.toLowerCase()}`}>
+          <li>{ page.title }</li>
+        </NavLink>
+      )}
+      </ul>
     </nav>
   )
 }
