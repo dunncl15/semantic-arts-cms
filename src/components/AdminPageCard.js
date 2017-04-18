1f878d6448f4781466023c946d970d2f6542353e
Charles Dunn <dunnc1551@gmail.com>
2017-04-17 18:16:27 -0600

    Adjust formatting on AdminPageCard for readability. Use location prop to conditionally render UserNavigationContainer.

--- src/components/AdminPageCard.js
+++ src/components/AdminPageCard.js
@@ -7,8 +7,12 @@ import cloudUnpublish from '../styles/images/cloud-unpublish.svg';
 import pencil from '../styles/images/pencil-blue.svg';
 import add from '../styles/images/add-icon.svg';
 
-const handleNav = ({ userNav, addToNav, removeFromNav, togglePublish, toggleNav, toggleNavPublish, title, content, id, published, navigation }) => {
+const handleNav = ({ userNav, addToNav, removeFromNav,
+                     togglePublish, toggleNav, toggleNavPublish,
+                     title, content, id, published, navigation }) => {
+
   const index = userNav.findIndex(page => page.title === title);
+
   if (index === -1 && !published) {
     addToNav({ title, content, id, published, navigation })
     toggleNavPublish({ title, content, id, published, navigation });
@@ -23,23 +27,29 @@ const handleNav = ({ userNav, addToNav, removeFromNav, togglePublish, toggleNav,
   }
 }
 
-const handlePublish = ({ userNav, removeFromNav, togglePublish, toggleNavPublish, title, content, id, published, navigation }) => {
+const handlePublish = ({ userNav, removeFromNav, togglePublish,
+                         toggleNavPublish, title, content,
+                         id, published, navigation }) => {
+
   const index = userNav.findIndex(page => page.title === title);
-  index !== -1 && published ? toggleNavPublish({ title, content, id, published, navigation }) : togglePublish({ title, content, id, published, navigation });
-  removeFromNav({ title, content, id, published, navigation });
+  index !== -1 && published ? toggleNavPublish({ title, content, id, published, navigation }) :
+                              (togglePublish({ title, content, id, published, navigation }),
+                               removeFromNav({ title, content, id, published, navigation }));
 }
 
-const AdminPageCard = ({ userNav, addToNav, removeFromNav, togglePublish, toggleNav, toggleNavPublish, title, content, id, published, navigation, history }) => {
-  const btnClass = classNames({
-    'unpublish': published,
-    'publish': !published
-  })
+const AdminPageCard = ({ userNav, addToNav, removeFromNav,
+                         togglePublish, toggleNav, toggleNavPublish,
+                         title, content, id, published, navigation, i }) => {
+
+  const btnClass = classNames({ 'unpublish': published, 'publish': !published })
   return (
     <article className='page-card' id={id}>
       <h3 className='page-title'>{ title }</h3>
       <div className='page-body' dangerouslySetInnerHTML={{__html: content}}></div>
       <button className={btnClass}
-              onClick={ () => handlePublish({ userNav, addToNav, removeFromNav, togglePublish, toggleNav, toggleNavPublish, title, content, id, published, navigation })}>
+              onClick={ () => handlePublish({ userNav, addToNav, removeFromNav,
+                                              togglePublish, toggleNav, toggleNavPublish,
+                                              title, content, id, published, navigation })}>
               { published ? <img className='cloud-icon' src={cloudUnpublish}/> : <img className='cloud-icon' src={cloudPublish}/>}
               { published ? 'Unpublish' : 'Publish' }
       </button>
@@ -47,10 +57,13 @@ const AdminPageCard = ({ userNav, addToNav, removeFromNav, togglePublish, toggle
         <Link className='edit' to={`pages/edit/${title}`}>
           <img className='pencil-blue' src={pencil}/>
           Edit page
-          </Link>
-        <button className='add-to-nav-btn' onClick={ () => handleNav({ userNav, addToNav, removeFromNav, togglePublish, toggleNav, toggleNavPublish, title, content, id, published, navigation }) }>
-        <img className='add-icon' src={add} />
-          { navigation ? <span>Remove from navigation</span> : <span>Add to navigation</span> }
+        </Link>
+        <button className='add-to-nav-btn'
+                onClick={ () => handleNav({ userNav, addToNav, removeFromNav,
+                                            togglePublish, toggleNav, toggleNavPublish,
+                                            title, content, id, published, navigation }) }>
+                <img className='add-icon' src={add} />
+                { navigation ? <span>Remove from navigation</span> : <span>Add to navigation</span> }
         </button>
         <p className='status'>
           <span>Status</span>: { published ? 'published' : 'unpublished' }
--- src/components/App.js
+++ src/components/App.js
@@ -5,27 +5,29 @@ import Navigation from './Navigation';
 import UserNavigationContainer from '../containers/UserNavigationContainer.js';
 import PagesContainer from '../containers/PagesContainer.js';
 import Page from './Page.js';
-import HeaderContainer from '../containers/HeaderContainer.js';
+import Header from './Header.js';
 import CreatePageContainer from '../containers/CreatePageContainer.js';
 
-const App = ({ pages, history }) => {
+const App = ({ pages, history, location }) => {
   return (
     <main className='App'>
-      <Route path='/' component={ UserNavigationContainer }></Route>
+      <Route path='/' render={ ({ location }) => {
+        return !location.pathname.includes('/admin') ? <UserNavigationContainer /> : null;
+      }}/>
       <Route path='/pages/:title' render={ ({ match }) => {
         const page = pages.find(page => page.title.toLowerCase() === match.params.title.toLowerCase());
         return page.published ? <Page page={page} /> : <div>Not Found</div>
-      }}></Route>
-        <Route path='/admin' component={ HeaderContainer }></Route>
+      }} />
+      <Route path='/admin' component={ Header } />
       <section className='main-content'>
-        <Route path='/admin' component={ Navigation }></Route>
-        <Route exact path='/admin/pages' component={ PagesContainer }></Route>
-        <Route exact path='/admin/new-page' component={ CreatePageContainer }></Route>
+        <Route path='/admin' component={ Navigation } />
+        <Route exact path='/admin/pages' component={ PagesContainer } />
+        <Route exact path='/admin/new-page' component={ CreatePageContainer } />
       </section>
       <Route path='/admin/pages/edit/:title' render={ ({ match }) => {
         const page = pages.find(page => page.title === match.params.title);
-        return <CreatePageContainer page={page} history={history}/>
-      }}></Route>
+        return <CreatePageContainer page={page} history={history} />
+      }} />
     </main>
   );
 }
