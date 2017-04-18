0c601bd8570c523214290eb18b0fb1a6ecc4f3c7
Charles Dunn <dunnc1551@gmail.com>
2017-04-18 10:42:07 -0600

    Pass index down as a prop to AdminPageCard.

--- src/components/Pages.js
+++ src/components/Pages.js
@@ -11,6 +11,7 @@ const Pages = ({ pages, userNav, history }) => {
       {pages.map((page, i) =>
         <AdminPageCardContainer
           {...page}
+          i={i}
           userNav={userNav}
           history={history}
           key={i}
