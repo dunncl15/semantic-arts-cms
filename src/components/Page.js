2867e48c922e0d11088cb0abd82c9d49cb349a67
Charles Dunn <dunnc1551@gmail.com>
2017-04-17 18:19:11 -0600

    Create page container to hold client facing page articles.

--- src/components/Page.js
+++ src/components/Page.js
@@ -2,10 +2,12 @@ import React from 'react';
 
 const Page = ({ page }) => {
   return (
-    <article id={page.id}>
-      <h2>{ page.title }</h2>
-      <div dangerouslySetInnerHTML={{__html: page.content}}></div>
-    </article>
+    <section className="pages-container">
+      <article id={page.id} className="public-page">
+        <h2>{ page.title }</h2>
+        <div dangerouslySetInnerHTML={{__html: page.content}}></div>
+      </article>
+    </section>
   )
 }
 
