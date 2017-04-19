d54d70073cced454d50ab45073668a925f8e479e
Charles Dunn <dunnc1551@gmail.com>
2017-04-18 18:58:36 -0600

    Add app tests.

--- src/tests/components/App.test.js
+++ src/tests/components/App.test.js
@@ -1,7 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow, mount } from 'enzyme';
+
 import App from '../../components/App';
 
-it('renders without crashing', () => {
-  
+describe('App', () => {
+  const wrapper = shallow(<App />)
+
+  it('should contain seven routes', () => {
+    expect(wrapper.find('Route').length).toEqual(7)
+  });
+
+  it('should contain a main-content section', () => {
+    expect(wrapper.find('.main-content').length).toEqual(1);
+  });
+
 });
